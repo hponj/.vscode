a75418bfeb26f0e59aea736b6e2aa91b0bda5408
hponj <mdj.faheem6@gmail.com>
2024-01-29 14:42:03 +0700

    c

--- shutdown.py
+++ shutdown.py
@@ -1,9 +1,8 @@
 import os
 import time
 
-def shutdown_after(seconds):
-    time.sleep(seconds)
+def shutdown_after(second):
+    time.sleep(second)
     os.system("shutdown /s /t 1")
 
-# Shutdown after 30 seconds
 shutdown_after(1)
